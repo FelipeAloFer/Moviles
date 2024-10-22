cd9a8bf22420b2c22c0f3dc433de329636b61c77
Felipe Alonso Fernández <felipe.alonsofernandez@iesvalleinclan.es>
2024-10-22 02:42:00 +0200

    21 de Oct Casa

--- Propuesta5_4/app/src/main/java/com/example/propuesta5_4/MainActivity3.java
+++ Propuesta5_4/app/src/main/java/com/example/propuesta5_4/MainActivity3.java
@@ -1,4 +1,27 @@
 package com.example.propuesta5_4;
 
-public class MainActivity3 {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
